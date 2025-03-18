558405d7684cc1a06aa0b6489ec900c4cb0b3cc1
alfred3ks <alfred3ks@gmail.com>
2025-03-18 21:00:05 +0100

    Instalo Tailwind para el css.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import CharacterList from './components/CharacterList';
 
+import './index.css';
+
 const App = () => {
   return (
     <div>
       <h1>Rick and Morty</h1>
+      <h1 className="text-5xl font-bold underline">Hello world!</h1>
       <CharacterList />
     </div>
   );
