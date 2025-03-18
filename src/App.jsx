1ded1b667b3d3496885d57c66bea129e1cd0838a
alfred3ks <alfred3ks@gmail.com>
2025-03-18 19:40:13 +0100

    Hago la petición a la API para traer los datos.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,40 @@
+import { useEffect, useState } from 'react';
+
 const App = () => {
-  return <h1>App</h1>;
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    const fetchApiRick = async () => {
+      try {
+        // hacemos la peticion a la API de rick and morty
+        const response = await fetch(
+          'https://rickandmortyapi.com/api/character'
+        );
+        const data = await response.json();
+        // console.log(data);
+        // console.log(data.results);
+        setCharacters(data.results);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchApiRick();
+  }, []);
+
+  return (
+    <div>
+      <h1>Rick and Morty</h1>
+
+      {characters.map((character) => {
+        return (
+          <div key={character.id}>
+            <h2>{character.name}</h2>
+            <img src={character.image} alt={`imagen de  ${character.name}`} />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default App;
